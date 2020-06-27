7eefb9bf12e93b33d5f7bcfc5dcaa153f715b2a0
Thais Gabriele <tgcn@icomp.ufam.edu.br>
2020-06-26 22:19:03 -0400

    pi1 v1

---
+++ pi1p1/src/main/java/br/edu/ufam/ThaisGabriele/App.java
@@ -0,0 +1,13 @@
+package br.edu.ufam.ThaisGabriele;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
